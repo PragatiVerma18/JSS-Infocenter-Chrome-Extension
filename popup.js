301dd163a37e6eeaf6433a2e57012e3dbcee90cb
pragati <itispragativerma@gmail.com>
2019-10-04 23:44:38 +0530

    fixed files

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-    $.get('http://210.212.85.155/', function(response){
+    $.get('http://210.212.85.155:8082/', function(response){
 
-        var notices, title, author, date;
+        var notices, title, link, date;
         notices = $('li.modal-index', response);
-        $('body').append('<ul id="notices"></ul>')
+        $('body').append('<ul id="notices" class="list is-hoverable"></ul>')
         $.each(notices, function(index, value){
             title = $('div.col-7', value).html();
-            author = $('div.auth-index', value).html();
+            //author = $('div.auth-index', value).html();
             date = $('div.date-index', value).html();
-            $('#notices').append('<li>' + title + '</li>');
+            $('#notices').append('<li class="list-item">' + title  + date +'</li>');
         });
 
     });
